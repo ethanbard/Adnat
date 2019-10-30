82fb6e783b764175fc2c31495154f1464053a14a
ethanbard <ethan.r.bard@gmail.com>
2019-10-30 16:52:22 -0500

    Users can now edit Organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,6 +3,13 @@ class OrganizationsController < ApplicationController
         @organizations = Organization.all
     end
 
+    def new
+    end
+
+    def edit
+        @organization = Organization.find(params[:id])
+    end
+
     def create
         @organization = Organization.new(organization_params)
 
@@ -10,20 +17,25 @@ class OrganizationsController < ApplicationController
         redirect_to @organization
     end
 
-    def new
-    end
-
-    def edit
-    end
-
     def show
         @organization = Organization.find(params[:id])
     end
 
     def update
+        @organization = Organization.find(params[:id])
+
+        if @organization.update(organization_params)
+            redirect_to @organization
+        else
+            render 'edit'
+        end
     end
 
     def destroy
+        @organization = Organization.find(params[:id])
+        @organization.destroy
+
+        redirect_to organizations_path
     end
 
     private
